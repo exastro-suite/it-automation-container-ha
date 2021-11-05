b94aba33a21dbbeed4e056fa2703936e7f0f4812
y-kikuchi2 <yoshiaki-kikuchi2@ncontr.com>
2021-11-05 09:40:13 +0900

    update sizai_20211105

--- 1.8.1/it-automation-backyard/centos8/build.sh
+++ 1.8.1/it-automation-backyard/centos8/build.sh
@@ -5,7 +5,6 @@
 
 curl -SL ${EXASTRO_ITA_INSTALLER_URL} | tar -xzC ${EXASTRO_ITA_UNPACK_BASE_DIR}
 
-
 ##############################################################################
 # Update all installed packages
 
@@ -14,3 +13,175 @@ dnf update -y
 
 ##############################################################################
 # Build
+##############################################################################
+# Set system locale and system timezone
+dnf -y --enablerepo=appstream install langpacks-ja
+dnf -y --enablerepo=appstream reinstall langpacks-en
+localectl set-locale "LANG=en_US.UTF-8"
+
+timedatectl set-timezone Asia/Tokyo
+
+##############################################################################
+# Python interpreter warning issue (container only)
+#   see https://docs.ansible.com/ansible/2.10/reference_appendices/interpreter_discovery.html
+
+find ${EXASTRO_ITA_UNPACK_BASE_DIR} | grep -E "/ansible.cfg$" | xargs sed -i -E 's/^\[defaults\]$/[defaults\]\ninterpreter_python=auto_silent/'
+
+#sizai_deploy
+cd ${EXASTRO_ITA_UNPACK_BASE_DIR}
+find it-automation-${EXASTRO_ITA_VER} -type f | xargs -I{} sed -i -e "s:%%%%%ITA_DIRECTORY%%%%%:${EXASTRO_ITA_INSTALL_DIR}:g" {}
+
+#mariadb_install
+yum install -y mariadb
+
+#php_install
+yum install -y yum-utils
+yum install -y epel-release
+yum config-manager --set-enabled powertools
+
+yum install -y php php-bcmath php-cli php-ldap php-mbstring php-mysqlnd php-pear php-pecl-zip php-process php-snmp php-xml zip telnet mailx unzip php-json php-gd python3 php-devel libyaml libyaml-devel make sudo crontabs
+
+pear install HTML_AJAX-beta
+
+ln -s /usr/share/pear-data/HTML_AJAX/js /usr/share/pear/HTML/js
+
+pecl channel-update pecl.php.net
+
+echo "" | pecl install YAML
+
+mkdir -p /usr/share/php/vendor
+
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin
+
+/usr/bin/composer.phar require "phpoffice/phpspreadsheet":"1.14.1"
+
+mv -f vendor /usr/share/php/
+
+#php_setting
+\cp -pf ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ext_files_for_CentOS8.x/etc/php.ini /etc/
+\cp -pf ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ext_files_for_CentOS8.x/etc_php-fpm.d/www.conf /etc/php-fpm.d/
+
+#ita_install
+mkdir -p ${EXASTRO_ITA_INSTALL_DIR}/data_relay_storage
+mkdir -p ${EXASTRO_ITA_INSTALL_DIR}/ita-root/webroot/menus
+mkdir -p ${EXASTRO_ITA_INSTALL_DIR}/ita-root/webconfs
+mkdir -p ${EXASTRO_ITA_INSTALL_DIR}/ita-root/confs
+
+mkdir -p /exastro-file-volume/data_relay_storage/ansible_driver
+mkdir -p /exastro-file-volume/data_relay_storage/conductor
+mkdir -p /exastro-file-volume/data_relay_storage/symphony
+mkdir -p /exastro-file-volume/ita-root/temp
+mkdir -p /exastro-file-volume/ita-root/uploadfiles
+mkdir -p /exastro-file-volume/ita-root/webroot/menus/sheets
+mkdir -p /exastro-file-volume/ita-root/webroot/menus/users
+mkdir -p /exastro-file-volume/ita-root/webconfs/sheets
+mkdir -p /exastro-file-volume/ita-root/webconfs/users
+mkdir -p /exastro-file-volume/ita-root/confs
+
+
+\cp -rpf ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-contents/ita-root/* ${EXASTRO_ITA_INSTALL_DIR}/ita-root
+\cp -rpf ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-confs/* ${EXASTRO_ITA_INSTALL_DIR}/ita-root/confs
+
+#mount_point
+ln -s /exastro-file-volume/data_relay_storage/ansible_driver ${EXASTRO_ITA_INSTALL_DIR}/data_relay_storage/ansible_driver
+ln -s /exastro-file-volume/data_relay_storage/conductor ${EXASTRO_ITA_INSTALL_DIR}/data_relay_storage/conductor
+ln -s /exastro-file-volume/data_relay_storage/symphony ${EXASTRO_ITA_INSTALL_DIR}/data_relay_storage/symphony
+
+ln -s /exastro-file-volume/ita-root/temp ${EXASTRO_ITA_INSTALL_DIR}/ita-root/temp
+ln -s /exastro-file-volume/ita-root/uploadfiles ${EXASTRO_ITA_INSTALL_DIR}/ita-root/uploadfiles
+ln -s /exastro-file-volume/ita-root/webroot/menus/sheets ${EXASTRO_ITA_INSTALL_DIR}/ita-root/webroot/menus/sheets
+ln -s /exastro-file-volume/ita-root/webroot/menus/users ${EXASTRO_ITA_INSTALL_DIR}/ita-root/webroot/menus/users
+ln -s /exastro-file-volume/ita-root/webconfs/sheets ${EXASTRO_ITA_INSTALL_DIR}/ita-root/webconfs/sheets
+ln -s /exastro-file-volume/ita-root/webconfs/users ${EXASTRO_ITA_INSTALL_DIR}/ita-root/webconfs/users
+
+#mkdir_create_dir_list.txt
+while read line
+do
+  mkdir -p ${EXASTRO_ITA_INSTALL_DIR}${line};
+done < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/install_scripts/list/create_dir_list.txt
+
+#chmod_755_list.txt
+while read line
+do
+  chmod 755 ${EXASTRO_ITA_INSTALL_DIR}${line};
+done < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/install_scripts/list/755_list.txt
+
+#chmod_777_list.txt
+while read line
+do
+  chmod 777 ${EXASTRO_ITA_INSTALL_DIR}${line};
+done < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/install_scripts/list/777_list.txt
+
+mv ${EXASTRO_ITA_INSTALL_DIR}/ita-root/webroot/uploadfiles /exastro-file-volume/ita-root/webroot
+ln -s /exastro-file-volume/ita-root/webroot/uploadfiles ${EXASTRO_ITA_INSTALL_DIR}/ita-root/webroot/uploadfiles
+
+cp -p ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-releasefiles/ita_base ${EXASTRO_ITA_INSTALL_DIR}/ita-root/libs/release/.
+cp -p ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-releasefiles/ita_createparam ${EXASTRO_ITA_INSTALL_DIR}/ita-root/libs/release/.
+cp -p ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-releasefiles/ita_hostgroup ${EXASTRO_ITA_INSTALL_DIR}/ita-root/libs/release/.
+cp -p ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-releasefiles/ita_ansible-driver ${EXASTRO_ITA_INSTALL_DIR}/ita-root/libs/release/.
+cp -p ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-releasefiles/ita_cobbler-driver ${EXASTRO_ITA_INSTALL_DIR}/ita-root/libs/release/.
+cp -p ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-releasefiles/ita_terraform-driver ${EXASTRO_ITA_INSTALL_DIR}/ita-root/libs/release/.
+
+#db_connect_setting
+echo -ne "mysql:dbname=ita_db;host=${EXASTRO_ITA_DB_CONTAINER_NAME}" | base64 | tr '[A-Za-z]' '[N-ZA-Mn-za-m]' > ${EXASTRO_ITA_INSTALL_DIR}/ita-root/confs/commonconfs/db_connection_string.txt
+
+echo -ne "${EXASTRO_ITA_DB_USERNAME}" | base64 | tr '[A-Za-z]' '[N-ZA-Mn-za-m]' > ${EXASTRO_ITA_INSTALL_DIR}/ita-root/confs/commonconfs/db_username.txt
+
+echo -ne "${EXASTRO_ITA_DB_PASSWORD}" | base64 | tr '[A-Za-z]' '[N-ZA-Mn-za-m]' > ${EXASTRO_ITA_INSTALL_DIR}/ita-root/confs/commonconfs/db_password.txt
+
+#backyard_setting
+ln -s ${EXASTRO_ITA_INSTALL_DIR}/ita-root/confs/backyardconfs/ita_env /etc/sysconfig/ita_env
+
+#service_base_service_list.txt
+while read line
+do
+  cp -p ${EXASTRO_ITA_INSTALL_DIR}${line}.service /usr/lib/systemd/system/.
+done < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/install_scripts/list/base_service_list.txt
+
+#service_createparam_service_list.txt
+while read line
+do
+  cp -p ${EXASTRO_ITA_INSTALL_DIR}${line}.service /usr/lib/systemd/system/.
+done < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/install_scripts/list/createparam_service_list.txt
+
+#service_hostgroup_service_list.txt
+while read line
+do
+  cp -p ${EXASTRO_ITA_INSTALL_DIR}${line}.service /usr/lib/systemd/system/.
+done < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/install_scripts/list/hostgroup_service_list.txt
+
+#service_ansible_service_list.txt
+while read line
+do
+  cp -p ${EXASTRO_ITA_INSTALL_DIR}${line}.service /usr/lib/systemd/system/.
+done < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/install_scripts/list/ansible_service_list.txt
+
+#service_createparam2_service_list.txt
+while read line
+do
+  cp -p ${EXASTRO_ITA_INSTALL_DIR}${line}.service /usr/lib/systemd/system/.
+done < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/install_scripts/list/createparam2_service_list.txt
+
+#service_cobbler_service_list.txt
+while read line
+do
+  cp -p ${EXASTRO_ITA_INSTALL_DIR}${line}.service /usr/lib/systemd/system/.
+done < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/install_scripts/list/cobbler_service_list.txt
+
+#service_terraform_service_list.txt
+while read line
+do
+  cp -p ${EXASTRO_ITA_INSTALL_DIR}${line}.service /usr/lib/systemd/system/.
+done < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/install_scripts/list/terraform_service_list.txt
+
+ls -1 /usr/lib/systemd/system/. | grep ky_ | xargs systemctl enable
+ls -1 /usr/lib/systemd/system/. | grep ky_ | xargs systemctl start
+
+#cron_setting
+cat << EOS > /var/spool/cron/root
+01 00 * * * su - -c ${EXASTRO_ITA_INSTALL_DIR}/ita-root/backyards/common/ky_execinstance_dataautoclean-workflow.sh'
+02 00 * * * su - -c ${EXASTRO_ITA_INSTALL_DIR}/ita-root/backyards/common/ky_file_autoclean-workflow.sh'
+EOS
+
+#git_install
+yum install -y git
--- 1.8.1/it-automation-mariadb/centos8/build.sh
+++ 1.8.1/it-automation-mariadb/centos8/build.sh
@@ -14,6 +14,21 @@ dnf update -y
 
 ##############################################################################
 # Build
+##############################################################################
+# Set system locale and system timezone
+dnf -y --enablerepo=appstream install langpacks-ja
+dnf -y --enablerepo=appstream reinstall langpacks-en
+localectl set-locale "LANG=en_US.UTF-8"
+
+timedatectl set-timezone Asia/Tokyo
+
+##############################################################################
+# Python interpreter warning issue (container only)
+#   see https://docs.ansible.com/ansible/2.10/reference_appendices/interpreter_discovery.html
+
+find ${EXASTRO_ITA_UNPACK_BASE_DIR} | grep -E "/ansible.cfg$" | xargs sed -i -E 's/^\[defaults\]$/[defaults\]\ninterpreter_python=auto_silent/'
+
+#sizai_deploy
 cd ${EXASTRO_ITA_UNPACK_BASE_DIR}
 find it-automation-${EXASTRO_ITA_VER} -type f | xargs -I{} sed -i -e "s:%%%%%ITA_DIRECTORY%%%%%:${EXASTRO_ITA_INSTALL_DIR}:g" {}
 
@@ -25,11 +40,7 @@ yum install -y mariadb mariadb-server expect
 systemctl enable mariadb
 systemctl start mariadb
 
-#dbuser_add
-SQL_DIR=/tmp
-DB_ROOT_PASSWORD=ita_root_password
-SECURE_COMMAND="mysql_secure_installation"
-
+#create dbuser
 cat << EOS > /tmp/create-db-and-user_for_MySQL.sql
 CREATE USER 'ITA_USER' IDENTIFIED BY 'ITA_PASSWD';
 CREATE USER 'ITA_USER'@'localhost' IDENTIFIED BY 'ITA_PASSWD';
@@ -38,59 +49,55 @@ GRANT ALL ON ITA_DB.* TO 'ITA_USER'@'%' WITH GRANT OPTION;
 GRANT ALL ON ITA_DB.* TO 'ITA_USER'@'localhost' WITH GRANT OPTION;
 EOS
 
-    if ! test -e "$SQL_DIR/create-db-and-user_for_MySQL.sql" ; then
-        log 'ERROR : create-db-and-user_for_MySQL.sql does not be found.'
-    else
-        sed -i -e "s/ITA_DB/${EXASTRO_ITA_DB_NAME}/g" $SQL_DIR/create-db-and-user_for_MySQL.sql
-        sed -i -e "s/ITA_USER/${EXASTRO_ITA_DB_USERNAME}/g" $SQL_DIR/create-db-and-user_for_MySQL.sql
-        sed -i -e "s/ITA_PASSWD/${EXASTRO_ITA_DB_PASSWORD}/g" $SQL_DIR/create-db-and-user_for_MySQL.sql
-        mysql -uroot < $SQL_DIR/create-db-and-user_for_MySQL.sql
-    fi
+sed -i -e "s/ITA_DB/${EXASTRO_ITA_DB_NAME}/g" /tmp/create-db-and-user_for_MySQL.sql
+sed -i -e "s/ITA_USER/${EXASTRO_ITA_DB_USERNAME}/g" /tmp/create-db-and-user_for_MySQL.sql
+sed -i -e "s/ITA_PASSWD/${EXASTRO_ITA_DB_PASSWORD}/g" /tmp/create-db-and-user_for_MySQL.sql
+mysql -uroot < /tmp/create-db-and-user_for_MySQL.sql
+
 rm -f /tmp/create-db-and-user_for_MySQL.sql
 
 # MariaDB (initialize)
-    # mariadb-secure-installation
-    send_db_root_password="${DB_ROOT_PASSWORD}"
-    send_db_root_password=$(echo "$send_db_root_password"|sed -e 's/\\/\\\\\\\\/g')
-    send_db_root_password=$(echo "$send_db_root_password"|sed -e 's/\$/\\\\\\$/g')
-    send_db_root_password=$(echo "$send_db_root_password"|sed -e 's/"/\\\\\\"/g')
-    send_db_root_password=$(echo "$send_db_root_password"|sed -e 's/\[/\\\\\\[/g')
-    send_db_root_password=$(echo "$send_db_root_password"|sed -e 's/\t/\\011/g')
+send_db_root_password="${EXASTRO_ITA_DB_ROOT_PASSWORD}"
+send_db_root_password=$(echo "$send_db_root_password"|sed -e 's/\\/\\\\\\\\/g')
+send_db_root_password=$(echo "$send_db_root_password"|sed -e 's/\$/\\\\\\$/g')
+send_db_root_password=$(echo "$send_db_root_password"|sed -e 's/"/\\\\\\"/g')
+send_db_root_password=$(echo "$send_db_root_password"|sed -e 's/\[/\\\\\\[/g')
+send_db_root_password=$(echo "$send_db_root_password"|sed -e 's/\t/\\011/g')
 
 #mysql_secure_installation
-        # Exactly say, MariaDB 10.4.2 or lower
-        expect -c "
-            set timeout -1
-            spawn ${SECURE_COMMAND}
-            expect \"Enter current password for root \\(enter for none\\):\"
-            send \"\\r\"
-            expect {
-                -re \"Switch to unix_socket authentication.* $\" {
-                    send \"n\\r\"
-                    expect -re \"Change the root password\\?.* $\"
-                    send \"Y\\r\"
-                }
-                -re \"Set root password\\?.* $\" {
-                    send \"Y\\r\"
-                }
-            }
-            expect \"New password:\"
-            send \""${send_db_root_password}\\r"\"
-            expect \"Re-enter new password:\"
-            send \""${send_db_root_password}\\r"\"
-            expect -re \"Remove anonymous users\\?.* $\"
+expect -c "
+    set timeout -1
+    spawn mysql_secure_installation
+    expect \"Enter current password for root \\(enter for none\\):\"
+    send \"\\r\"
+    expect {
+        -re \"Switch to unix_socket authentication.* $\" {
+            send \"n\\r\"
+            expect -re \"Change the root password\\?.* $\"
             send \"Y\\r\"
-            expect -re \"Disallow root login remotely\\?.* $\"
+        }
+        -re \"Set root password\\?.* $\" {
             send \"Y\\r\"
-            expect -re \"Remove test database and access to it\\?.* $\"
-            send \"Y\\r\"
-            expect -re \"Reload privilege tables now\\?.* $\"
-            send \"Y\\r\""
+        }
+    }
+    expect \"New password:\"
+    send \""${send_db_root_password}\\r"\"
+    expect \"Re-enter new password:\"
+    send \""${send_db_root_password}\\r"\"
+    expect -re \"Remove anonymous users\\?.* $\"
+    send \"Y\\r\"
+    expect -re \"Disallow root login remotely\\?.* $\"
+    send \"Y\\r\"
+    expect -re \"Remove test database and access to it\\?.* $\"
+    send \"Y\\r\"
+    expect -re \"Reload privilege tables now\\?.* $\"
+    send \"Y\\r\""
 
+#copy server.cnf
 cp -p ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ext_files_for_CentOS8.x/etc_my.cnf.d/server.cnf /etc/my.cnf.d/server.cnf
-
 systemctl restart mariadb
 
+#create table
 mysql -u ${EXASTRO_ITA_DB_USERNAME} -p${EXASTRO_ITA_DB_PASSWORD} -D ${EXASTRO_ITA_DB_NAME} < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-sqlscripts/ja_JP_mysql_ita_model-a.sql
 mysql -u ${EXASTRO_ITA_DB_USERNAME} -p${EXASTRO_ITA_DB_PASSWORD} -D ${EXASTRO_ITA_DB_NAME} < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-sqlscripts/ja_JP_mysql_ita_model-c.sql
 mysql -u ${EXASTRO_ITA_DB_USERNAME} -p${EXASTRO_ITA_DB_PASSWORD} -D ${EXASTRO_ITA_DB_NAME} < ${EXASTRO_ITA_UNPACK_BASE_DIR}/it-automation-${EXASTRO_ITA_VER}/ita_install_package/ITA/ita-sqlscripts/ja_JP_mysql_ita_model-d.sql
